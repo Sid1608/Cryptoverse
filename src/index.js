e0153e31c1e0a5ced2f256ad637755cf68a0690a
Sid1608 <akarsiddharth@gmail.com>
2022-01-21 16:59:04 +0530

    added redux toolkit, built api

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // index.js is the starting point of any new react application
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
-import 'antd/dist/antd.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./app/store";
+import "antd/dist/antd.css";
 ReactDOM.render(
-    <Router>
-        <App/>
-    </Router>
-    ,document.getElementById('root'));
+  <Router>
+  {/* our entire app is inside of a Provider this means that each and every component of this app is going to have acces to store variable  */}
+    <Provider store={store}>
+        <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
+);
